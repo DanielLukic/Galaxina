95dc1b103498f3e3caa0b57103b91ed75d7808f7
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-17 04:39:53 +0100

    Implemented simple music fading in MusicController.

--- src/net/intensicode/galaxina/MusicController.java
+++ src/net/intensicode/galaxina/MusicController.java
@@ -2,7 +2,7 @@ package net.intensicode.galaxina;
 
 import net.intensicode.core.AudioResource;
 import net.intensicode.screens.ScreenBase;
-import net.intensicode.util.Assert;
+import net.intensicode.util.*;
 
 public class MusicController extends ScreenBase
     {
@@ -12,31 +12,78 @@ public class MusicController extends ScreenBase
         Assert.notNull( "valid music id", aMusicId );
         //#endif
         if ( myActiveMusicId == aMusicId ) return;
-        if ( myActiveMusicResource != null ) myActiveMusicResource.stop();
-        myActiveMusicResource = audio().loadMusic( aMusicId );
-        myActiveMusicId = aMusicId;
+        if ( myState == STATE_IDLE ) playNewMusic( aMusicId );
+        else if ( myState == STATE_PLAYING ) fadeIntoNewMusic( aMusicId );
         }
 
     public final void stop()
         {
-        if ( myActiveMusicResource == null ) return;
-        myActiveMusicResource.stop();
+        if ( myActiveMusicResource != null ) myActiveMusicResource.stop();
         myActiveMusicResource = null;
-        myActiveMusicId = null;
+        myActiveMusicId = myScheduledMusicId = null;
+        myState = STATE_IDLE;
         }
 
     // From ScreenBase
 
     public final void onControlTick() throws Exception
         {
+        if ( myState == STATE_FADE_OUT )
+            {
+            final int fadeTicks = timing().ticksPerSecond;
+            if ( myFadeTick < fadeTicks )
+                {
+                myFadeTick++;
+                final int newVolume = MUSIC_VOLUME_IN_PERCENT - myFadeTick * MUSIC_VOLUME_IN_PERCENT / fadeTicks;
+                myActiveMusicResource.setVolume( newVolume );
+                }
+            else
+                {
+                playNewMusic( myScheduledMusicId );
+                myScheduledMusicId = null;
+                }
+            }
         }
 
     public final void onDrawFrame()
         {
         }
 
+    // Implementation
+
+    private void playNewMusic( final String aMusicId )
+        {
+        if ( myActiveMusicResource != null ) myActiveMusicResource.stop();
+        myActiveMusicResource = audio().loadMusic( aMusicId );
+        myActiveMusicResource.setVolume( MUSIC_VOLUME_IN_PERCENT );
+        myActiveMusicResource.start();
+        myActiveMusicId = aMusicId;
+        myState = STATE_PLAYING;
+        }
+
+    private void fadeIntoNewMusic( final String aMusicId )
+        {
+        myScheduledMusicId = aMusicId;
+        myFadeTick = 0;
+        myState = STATE_FADE_OUT;
+        }
+
+
+    private int myFadeTick;
+
+    private int myState = STATE_IDLE;
 
     private String myActiveMusicId;
 
+    private String myScheduledMusicId;
+
     private AudioResource myActiveMusicResource;
+
+    private static final int STATE_IDLE = 0;
+
+    private static final int STATE_PLAYING = 1;
+
+    private static final int STATE_FADE_OUT = 2;
+
+    private static final int MUSIC_VOLUME_IN_PERCENT = 75;
     }
