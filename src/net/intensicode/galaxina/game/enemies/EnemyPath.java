150ba6ffba40485bc92b2733bfa883a93138d079
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-22 02:56:44 +0100

    Code style fixed in EnemyPath.

--- src/net/intensicode/galaxina/game/enemies/EnemyPath.java
+++ src/net/intensicode/galaxina/game/enemies/EnemyPath.java
@@ -1,7 +1,3 @@
-/************************************************************************/
-/* {{PROJECT_NAME}}             {{COMPANY}}             {{DATE_CREATE}} */
-/************************************************************************/
-
 package net.intensicode.galaxina.game.enemies;
 
 import net.intensicode.galaxina.game.objects.World;
@@ -9,40 +5,35 @@ import net.intensicode.path.SmoothPath;
 import net.intensicode.util.Position;
 
 
-
-/**
- * TODO: Describe this!
- */
 public final class EnemyPath extends SmoothPath
-{
-    public EnemyPath( final World aWorld )
     {
+    public EnemyPath( final World aWorld )
+        {
         myWorld = aWorld;
-    }
+        }
 
     public final EnemyPath addRelativePos( final int aX, final int aY )
-    {
+        {
         addWorldPos( getWorldPos( aX, aY ) );
         return this;
-    }
+        }
 
     public final EnemyPath addWorldPos( final Position aWorldPosition )
-    {
+        {
         add( aWorldPosition );
         return this;
-    }
+        }
 
     // Implementation
 
-    private final Position getWorldPos( final int aX, final int aY )
-    {
+    private Position getWorldPos( final int aX, final int aY )
+        {
         myTempPos.setTo( myWorld.relativeToWorld( aX, aY ) );
         return myTempPos;
-    }
-
+        }
 
 
     private final World myWorld;
 
     private final Position myTempPos = new Position();
-}
+    }
