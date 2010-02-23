845e71d172717fca53e52dabdec989c2934783df
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-23 22:46:00 +0100

    Added version string using minifont to TitleScreen.

--- src/net/intensicode/galaxina/screens/TitleScreen.java
+++ src/net/intensicode/galaxina/screens/TitleScreen.java
@@ -3,7 +3,8 @@ package net.intensicode.galaxina.screens;
 import net.intensicode.core.ImageResource;
 import net.intensicode.galaxina.MainContext;
 import net.intensicode.graphics.FontGenerator;
-import net.intensicode.screens.ImageScreen;
+import net.intensicode.screens.*;
+import net.intensicode.ReleaseProperties;
 
 public final class TitleScreen extends GalaxinaScreen
     {
@@ -18,7 +19,7 @@ public final class TitleScreen extends GalaxinaScreen
         {
         final ImageResource title = skin().image( "title_logo" );
         final int titlePosX = ( screen().width() - title.getWidth() ) / 2;
-        final int titlePosY = title.getHeight() / 2;
+        final int titlePosY = title.getHeight() * 2;
 
         final FontGenerator creditsFont = skin().font( "menufont" );
         final String creditsText = resources().loadString( "credits.txt" );
@@ -27,6 +28,15 @@ public final class TitleScreen extends GalaxinaScreen
         addScreen( new ImageScreen( title, titlePosX, titlePosY, ImageScreen.MODE_ABSOLUTE ) );
         addScreen( new TitleCreditsScreen( creditsFont, creditsText ) );
         addScreen( visuals().sharedSoftkeys() );
+
+        final StringBuffer versionTag = new StringBuffer();
+        versionTag.append( "Version: " );
+        versionTag.append( ReleaseProperties.VERSION );
+        versionTag.append( "/" );
+        versionTag.append( ReleaseProperties.DATE );
+
+        final FontGenerator textFont = skin().font( "minifont" );
+        addScreen( new AlignedTextScreen( textFont, versionTag.toString(), 0, 0, FontGenerator.TOP_LEFT ) );
 
         context().musicController().play( "theme" );
         }
