e064cde64e6dee9072ec709f5fa57754cedd83c1
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-26 17:47:08 +0100

    Activated DEBUG_OPENGL for Androgina_DEBUG configuration.

--- config/Androgina_DEBUG.rb
+++ config/Androgina_DEBUG.rb
@@ -11,7 +11,7 @@
 
 # Use this to activate global - not target-specific - settings. Used
 # primarly for debug output and effects.
-@symbols = [ :FULL_FX, :DEBUG ]
+@symbols = [ :FULL_FX, :DEBUG, :DEBUG_OPENGL ]
 
 # Force mod music format:
 @properties['target.music_suffix'] = '.mod'
