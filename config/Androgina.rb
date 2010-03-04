adabed7e7e921999828cae943851695cef0e2872
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-03-04 23:41:25 +0100

    Cleaned up Androgina build configurations by loading base Androgina.rb in other configurations.

--- config/Androgina.rb
+++ config/Androgina.rb
@@ -11,7 +11,7 @@
 
 # Use this to activate global - not target-specific - settings. Used
 # primarly for debug output and effects.
-@symbols = [ :FULL_FX ]
+@symbols = [ :FULL_FX, :SENSORS ]
 
 # Force mod music format:
 @properties['target.music_suffix'] = '.mod'
--- config/Androgina_OPENGL.rb
+++ config/Androgina_OPENGL.rb
@@ -1,18 +1,3 @@
-# Name of the resulting jar/jad bundle (no special chars).
-@name = "Galaxina"
+load_config 'Androgina.rb'
 
-# Possible targets are defined in file _targets.rb.
-@targets = [ Opengl_ANDROID ]
-
-# Possible sizes correspond to the image folders in the res directory.
-# You may use arbitrary folder names (no spaces or special chars) here -
-# as long as they exist.
 @sizes = [ "OpenGL" ]
-
-# Use this to activate global - not target-specific - settings. Used
-# primarly for debug output and effects.
-@symbols = [ :FULL_FX ]
-
-# Force mod music format:
-@properties['target.music_suffix'] = '.mod'
-@properties['target.music_type'] = 'audio/mod'
