82e8578021942f5ac4b12c3803795a5a261cb117
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-12-15 13:10:10 +0100

    Fixed UtilitiesEx#directionToDegrees after it got destroyed while switching to float-based 'path' package.

--- src/net/intensicode/galaxina/util/UtilitiesEx.java
+++ src/net/intensicode/galaxina/util/UtilitiesEx.java
@@ -7,29 +7,29 @@ public final class UtilitiesEx
     public static int getDirectionID( final float aDirectionInDegrees, final int aDirectionSteps )
         {
         final float directionStep = 360 / aDirectionSteps;
-        return (int) (( aDirectionInDegrees + directionStep / 2 ) / directionStep);
+        return (int) ( ( aDirectionInDegrees + directionStep / 2 ) / directionStep );
         }
 
     public static float directionToDegrees( final PositionF aDirection )
         {
-        final float length = MathExtended.length( aDirection.x, aDirection.y );
-        final float angleFP = (float) MathExtended.toDegrees( MathExtended.acos( length ) );
+        final float xNormalized = aDirection.x / MathExtended.length( aDirection.x, aDirection.y );
+        final float angleInDegrees = (float) MathExtended.toDegrees( MathExtended.acos( xNormalized ) );
 
         if ( aDirection.x < 0 && aDirection.y <= 0 )
             {
-            return 360 + 90 - angleFP;
+            return 360 + 90 - angleInDegrees;
             }
         if ( aDirection.x < 0 && aDirection.y >= 0 )
             {
-            return 90 + angleFP;
+            return 90 + angleInDegrees;
             }
         if ( aDirection.x >= 0 && aDirection.y <= 0 )
             {
-            return 90 - angleFP;
+            return 90 - angleInDegrees;
             }
         if ( aDirection.x >= 0 && aDirection.y >= 0 )
             {
-            return 90 + angleFP;
+            return 90 + angleInDegrees;
             }
         else
             {
